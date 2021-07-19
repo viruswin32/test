95e0f2a836812946d15b2de757763829f78b3f30
viruswin32 <zotkinsergey@gmail.com>
2021-07-19 12:46:57 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,26 +1,21 @@
-SET TYPE=Release
-SET TYPE=Debug
+SET TYPE=%CONFIGURATION%
+SET DEVSPACE=%APPVEYOR_BUILD_FOLDER%\..
+cd %DEVSPACE%
+Invoke-WebRequest http://draconpern-buildcache.s3.amazonaws.com/dcmtk-win.7z -Outfile "dcmtk-win.7z"
+7z x -y dcmtk-win.7z
+cd %DEVSPACE%
+git clone --branch=master https://github.com/uclouvain/openjpeg.git
+cd openjpeg && git checkout openjpeg-2.1 && mkdir build-%TYPE% && cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_THIRDPARTY=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\openjpeg\%TYPE%
+msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
 
-REM a top level directory for all PACS related code
-SET DEVSPACE="%CD%"
+cd %APPVEYOR_BUILD_FOLDER% && mkdir build-%TYPE% && cd build-%TYPE%
+cmake .. -G "Visual Studio 11" -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DOPENJPEG=%DEVSPACE%\openjpeg\%TYPE% -DDCMTK_DIR=%DEVSPACE%\dcmtk\%TYPE% -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%\%TYPE%
 
-cd %DEVSPACE%\dcmtk
-mkdir build-%TYPE%
-cd build-%TYPE%
-cmake .. -G "Visual Studio 11" -DDCMTK_WIDE_CHAR_FILE_IO_FUNCTIONS=1 -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\dcmtk\%TYPE%
-msbuild /P:Configuration=%TYPE% INSTALL.vcxproj 
-cd ..\..
-
-cd %DEVSPACE%\openjpeg
-mkdir build-%TYPE%
-cd build-%TYPE%
-cmake .. -G "Visual Studio 11" -DBUILD_THIRDPARTY=1 -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_C_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\openjpeg\%TYPE%
+cd %APPVEYOR_BUILD_FOLDER%\build-%TYPE%
 msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
 
 cd %DEVSPACE%
-mkdir build-%TYPE%
-cd build-%TYPE%
-cmake .. -G "Visual Studio 11" -DBUILD_SHARED_LIBS=OFF -DCMAKE_CXX_FLAGS_RELEASE="/MT /O2 /D NDEBUG" -DCMAKE_CXX_FLAGS_DEBUG="/D_DEBUG /MTd /Od" -DOPENJPEG=%DEVSPACE%\openjpeg\%TYPE% -DDCMTK_DIR=%DEVSPACE%\dcmtk\build-%TYPE% -DCMAKE_INSTALL_PREFIX=%DEVSPACE%\%TYPE%
-msbuild /P:Configuration=%TYPE% INSTALL.vcxproj
+7z a fmjpeg2koj-win.7z fmjpeg2koj\%TYPE% openjpeg\%TYPE%
 
-cd %DEVSPACE%
+mv fmjpeg2koj-win.7z %APPVEYOR_BUILD_FOLDER%
